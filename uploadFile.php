afdf4360923624c95aa21ba21edd9532fbe368ca
arunko350 <arunko350@gmail.com>
2012-12-06 13:22:02 +0530

    demo and download links given

--- uploadFile.php
+++ uploadFile.php
@@ -4,7 +4,18 @@
     if(mkdir("projects/".$name, 0777)){
         $path = "projects/".$name."/".$name.".zip";
         if(move_uploaded_file($tmp,$path)){
-             echo "<strong>Congratulations! </strong>  Your project is uploaded successfully.";
+              $zip = new ZipArchive;
+              $res = $zip->open($path);
+             if ($res == TRUE) {
+                 mkdir("projects/".$name."/demo", 0777);
+                 $zip->extractTo('projects/'.$name.'/demo');
+                 $zip->close();
+                echo "<strong>Congratulations! </strong>  Your project is uploaded successfully.  <a href='http://localhost/projecttub/projects/$name/demo'> Demo </a>    <a href='http://localhost/projecttub/projects/$name/$name.zip'> Download </a>";
+             } 
+             else {
+                echo "<strong>Oops! </strong>  We're sorry, but something went wrong. Please try again.";
+             }
+             
         }
 
         else{
